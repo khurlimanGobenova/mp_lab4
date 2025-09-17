18a8ebc78d4be02a1a4962007a3ec01c36a87f97
khurlimanGobenova <147365037+khurlimanGobenova@users.noreply.github.com>
2025-09-17 18:45:04 +0500

    4 sections complete

--- main.dart
+++ main.dart
@@ -150,6 +150,117 @@ class Manager extends Employee(){
     double bonus;
     @override
 }
+//3.4 Employee and Manager
+class Employee(){
+    String name;
+    double baseSalary;
+    Employee(this.name, this.baseSalary);
+    
+}
+class Manager extends Employee(){
+    double bonus;
+    Manager(String name, double baseSalary, this.bonus): super(name,baseSalary);
+
+    @override
+    double calculateSalary(){
+        return baseSalary+bonus;
+    }
+}
+
+//3.5 Library items
+class LibraryItem(){
+    String title;
+    String id;
+    LibraryItem(this.title, this.id);
+    String getDetails() {
+        return 'Title: $title, ID: $id';
+    }
+}
+class Book extends LibraryItem(){
+    String author;
+    Book(this.author, String title, String id): super(title, id);
+    @override
+    String getDetails(){
+        return "Book Title: $title, ID: $id, Author: $author";
+    }
+}
+class DVD extends LibraryItem(){
+    String director;
+    DVD(this.director, String title, String id): super(title, id);
+    @override
+    String getDetails(){
+        return "DVD Title: $title, ID: $id, Author: $author";
+    }
+}
+
+//MIXINS
+//4.2 Flyer mixin
+mixin Flyer(){
+    void swim(){
+        print("Taking off!");
+    }
+}
+class Bird with Flyer(){
+    String species;
+    Bird(this.species);
+}
+class Plane with Flyer(){
+    String model;
+    Plane(this.model);
+}
+
+//4.3 Logger Mixin
+mixin Logger(){
+    void log(String message){
+        print("[LOG] $message");
+    }
+}
+class DataService with Logger(){
+    void fetchData() {
+    log("Fetching data");
+    print("Data fetched successfully");
+  }
+}
+
+//4.4 Validator mixin
+mixin Validator(){
+    bool isEmailValid(String email){
+        return email.contains('@') && email.contains('.');
+    }
+    bool isPasswordStrong(String password){
+        if(password.length()>=8){
+            return true;
+        }
+        else return false
+    }
+}
+class UserSignUp with Validator(){
+    void validateUser(String email, String password){
+        print("Email valid: ${isEmailValid(email)}");
+        print("Password strong: ${isPasswordStrong(password)}");
+    }
+}
+//4.5 Combining Mixins
+mixin Performer {
+  void perform() {
+    print("Performing on stage!");
+  }
+}
+mixin Musician {
+  void playInstrument() {
+    print("Playing an instrument!");
+  }
+}
+class Artist {
+  String name;
+  Artist(this.name);
+}
+class Dancer extends Artist with Performer {
+  Dancer(String name) : super(name);
+}
+class Guitarist extends Artist with Performer, Musician {
+  Guitarist(String name) : super(name);
+}
 //MAIN FUNCTION
 void main(){
     //1.3 Book class
@@ -160,13 +271,73 @@ void main(){
     print("Name: ${student.name}, Age:${student.age}");
     print("Average grade: ${student.calculateAverageGrade().toString()}");
     //1.4: Bank Account
-    var account = BankAccount('ACC001', 'Jane Smith', 1000.0);
+    var account = BankAccount("ACC12", "Anya Pokrov", 1000.0);
     account.deposit(500.0);
     account.withdraw(200.0);
     account.withdraw(1500.0);
     //1.5 Rectangle class with getters
     var rectangle = Rectangle(15.3,74.3);
-    print('Width: ${rectangle.width}, Height: ${rectangle.height}');
-    print('Area: ${rectangle.area}');
-    print('Perimeter: ${rectangle.perimeter}');
+    print("Width: ${rectangle.width}, Height: ${rectangle.height}");
+    print("Area: ${rectangle.area}");
+    print("Perimeter: ${rectangle.perimeter}");
+    //ENUMS
+    //2.2 Order status
+    OrderStatus myOrder = OrderStatus.processing;
+    print("Order status: $myOrder"                                                                      );
+    //2.3 Traffic light
+    for (TrafficLight light in TrafficLight.values) {
+        print('$light: ${getTrafficLightAction(light)}');
+    }
+    //2.4 User Roles
+    var admin = User("Amina", UserRole.admin);
+    var editor = User("Bobur", UserRole.editor);
+    var guest = User("Madina", UserRole.guest);
+    print('${admin.name} (${admin.role}) has edit permission: ${admin.hasEditPermission()}');
+    print('${editor.name} (${editor.role}) has edit permission: ${editor.hasEditPermission()}');
+    print('${guest.name} (${guest.role}) has edit permission: ${guest.hasEditPermission()}');
+    //2.5 Traffic light extension
+    for (TrafficLight light in TrafficLight.values) {
+        print("${light.name}: ${light.durationInSeconds} seconds");
+    }
+    //INHERITANCE
+    //3.2 Animal sounds
+    var dog = Dog();
+    var cat = Cat();
+    dog.makeSound();
+    cat.makeSound();
+    //3.3 Shape hierarchy
+    var circle = Circle("purple", 5.0);
+    var square = Square("yellow", 4.0);
+    print("Circle: ${circle.color}, radius: ${circle.radius}");
+    print("Square: ${square.color}, side: ${square.sideLength}");
+    //3.4 Employee and Manager
+    var employee = Employee("John", 50000);
+    var manager = Manager("Tom", 60000, 15000);
+    print('${employee.name} salary: \$${employee.calculateSalary()}');
+    print('${manager.name} salary: \$${manager.calculateSalary()}');
+    //3.5 Library Items
+     var bookItem = BookItem("After the funeral", "A001", "Agatha Cristie");
+    var dvd = DVD("Interstellar", "I102", "Christopher Nolan");
+    print(bookItem.getDetails());
+    print(dvd.getDetails());
+    
+    //MIXINS
+    //4.2 Flyer Mixin
+    var bird = Bird("Crow");
+    var plane = Plane("Airbus Neo 320");
+    bird.fly();
+    plane.fly();
+    //4.3 Logger Mixin
+    var dataService = DataService();
+    dataService.fetchData();
+    // Task 4.4: Validator Mixin
+    var userSignup = UserSignup();
+    userSignup.validateUser("someone123@newuu.uz", "qwerty12");
+    userSignup.validateUser("invalid_email", "weak");
+    //4.5 Combining Mixins
+    var dancer = Dancer("Camelia");
+    var guitarist = Guitarist("Timothy");
+    dancer.perform();
+    guitarist.perform();
+    guitarist.playInstrument();
 }
