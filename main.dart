7c7a1d2d0e2f55210df100e7acaaf310d930e68c
Xurliman Gobenova <x.gobenova@newuu.uz>
2025-09-17 10:57:04 +0500

    section 3 complete

--- main.dart
+++ main.dart
@@ -60,8 +60,95 @@ class Rectangle{
 
     Rectangle(this.width, this.height);
     
+    double get area => width*height;
+    double get perimter => (width + height)*2;
+}
 
+//ENUMS
+//2.2 Order Status
+enum OrderStatus{pending,processing,shipped,delivered,cancelled }
 
+//2.3 Traffic Light 
+enum TrafficLight{red, yellow, green}
+String getTrafficLightAction(TrafficLight light) {
+  switch (light) {
+    case TrafficLight.red:
+      return "Stop";
+    case TrafficLight.yellow:
+      return "Caution";
+    case TrafficLight.green:
+      return "Go";
+  }
+}
+
+//2.4 User Roles
+enum UserRole{admin,editor,guest}
+class User(){
+    String name;
+    UserRole role;
+
+    bool hasEditPermission(){
+        retutn role == UserRole.admin || role == UserRole.editor;
+    }
+}
+
+//2.5Enums with Properties
+enum TrafficLightDuration(red,yellow,green)
+int get durationInSeconds {
+    switch (this) {
+      case TrafficLight.red:
+        return 30;
+      case TrafficLight.yellow:
+        return 5;
+      case TrafficLight.green:
+        return 45;
+    }
+  }
+
+//INHERITANCE
+//3.2 Animal sounds
+class Animal(){
+    void makeSound(){
+        print("Animal Sound");
+    }
+}
+class Dog extends Animal(){
+    @override
+    void makeSound(){
+        print("Woof");
+    }
+}
+class Cat extends Animal(){
+    @override
+    void makeSound(){
+        print("Meow");
+    }
+}
+
+//3.3 Shape hierarchy
+class Shape(){
+    String color;
+    Shape(this.color);
+}
+class Circle extends Shape(){
+    double radius;
+    Circle(this.radius, String color):super(color);
+}
+class Square extends Shape(){
+    double sideLength;
+    Square(this.sideLength, String color):super(color);
+}
+
+//3.4 Employee and Manager
+class Employee(){
+    String name;
+    double baseSalary;
+    Employee(this.name, this.baseSalary);
+    
+}
+class Manager extends Employee(){
+    double bonus;
+    @override
 }
 //MAIN FUNCTION
 void main(){
@@ -72,9 +159,14 @@ void main(){
     var student = Student("Tom", 19, [85.2,76.9,90.4,98.2]);
     print("Name: ${student.name}, Age:${student.age}");
     print("Average grade: ${student.calculateAverageGrade().toString()}");
-    //Task 1.4: Bank Account
+    //1.4: Bank Account
     var account = BankAccount('ACC001', 'Jane Smith', 1000.0);
     account.deposit(500.0);
     account.withdraw(200.0);
     account.withdraw(1500.0);
+    //1.5 Rectangle class with getters
+    var rectangle = Rectangle(15.3,74.3);
+    print('Width: ${rectangle.width}, Height: ${rectangle.height}');
+    print('Area: ${rectangle.area}');
+    print('Perimeter: ${rectangle.perimeter}');
 }
